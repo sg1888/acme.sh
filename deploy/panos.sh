bca56b69b53a080781a563e58c16e1f9bd462926
sg1888 <ssg1888@nyu.edu>
2023-07-20 16:23:27 +0000

    Added capability to simultaneously deploy to multiple hosts

--- deploy/panos.sh
+++ deploy/panos.sh
@@ -8,9 +8,19 @@
 # Firewall admin with superuser and IP address is required.
 #
 # REQURED:
-#     export PANOS_HOST=""
+#     export PANOS_HOST=""    #Can be a single host, or multiple hosts delimited by a comma or space.
 #     export PANOS_USER=""    #User *MUST* have Commit and Import Permissions in XML API for Admin Role
 #     export PANOS_PASS=""
+#
+# EXAMPLE:
+#     # You can use any of the following methods to export a host
+#     export PANOS_HOST="fw1.mydomain.com"                                         # Single host
+#     export PANOS_HOST="fw1.mydomain.com, fw2.mydomain.com, 10.23.1.37"           # Valid multi-host
+#     export PANOS_HOST="fw1.mydomain.com fw2.mydomain.com 10.8.8.22"              # Also vaild
+#     export PANOS_HOST=",,   fw1.mydomain.com   , 172.15.2.3,fw3.mydomain.com,"   # This works as well! (But WHY?)
+#
+#     export PANOS_USER="svc_acmeuser"
+#     export PANOS_PASS="a089s98qasdjfadfasdsdf"
 #
 # The script will automatically generate a new API key if
 # no key is found, or if a saved key has expired or is invalid.
@@ -119,7 +129,7 @@ deployer() {
     return 0
   else
     _err "Deploy of type $type failed. Try deploying with --debug to troubleshoot."
-    _debug "$message"
+    _err "$message"
     return 1
   fi
 }
@@ -129,6 +139,8 @@ panos_deploy() {
   _cdomain=$(echo "$1" | sed 's/*/WILDCARD_/g') #Wildcard Safe Filename
   _ckey="$2"
   _cfullchain="$5"
+  _regen_keys=false #Flag to regenerate keys if PANOS_USER or PANOS_PASS changes.
+  _old_host=""      #Empty string placeholder for old hosts
 
   # VALID FILE CHECK
   if [ ! -f "$_ckey" ] || [ ! -f "$_cfullchain" ]; then
@@ -136,56 +148,77 @@ panos_deploy() {
     return 1
   fi
 
-  # PANOS_HOST
+  ## PANOS_HOST - Can be a single host, or a comma delimited string of multiple hosts
   if [ "$PANOS_HOST" ]; then
-    _debug "Detected ENV variable PANOS_HOST. Saving to file."
-    _savedeployconf PANOS_HOST "$PANOS_HOST" 1
+    _debug "Detected ENV variable PANOS_HOST. Looking for changes..."
+    # Convert ENV variable to  lowercase, sort and remove duplicates, eliminate excessive spaces then put onto new lines
+    PANOS_ENV_HOST=$(echo "$PANOS_HOST" | tr '[:upper:]' '[:lower:]' | tr ' ' ',' | tr ',' '\n' | sort -u | tr '\n' ' ' | tr -s ' ' | sed 's/^[ \t]*//g' | tr ' ' '\n')
+    unset PANOS_HOST
+    #Attempt to load saved variable
+    _getdeployconf PANOS_HOST
+    _old_host="$PANOS_HOST" #Save the old hosts variable for later cleanup
+    if [ -z "$PANOS_HOST" ] || { [ "$PANOS_HOST" ] && [ "$PANOS_ENV_HOST" != "$PANOS_HOST" ]; }; then
+      _debug "PANOS_HOST has changed.  Saving changes to disk."
+      _savedeployconf PANOS_HOST "$PANOS_ENV_HOST" 1 #Save the new hosts to file
+      PANOS_HOST="$PANOS_ENV_HOST"                   #Replace the variable
+    else
+      _debug "PANOS_HOST is unchanged."
+    fi
   else
     _debug "Attempting to load variable PANOS_HOST from file."
     _getdeployconf PANOS_HOST
+    _old_host="$PANOS_HOST" #Save the old hosts variable for later cleanup
   fi
 
-  # PANOS USER
+  ## PANOS USER
   if [ "$PANOS_USER" ]; then
-    _debug "Detected ENV variable PANOS_USER. Saving to file."
-    _savedeployconf PANOS_USER "$PANOS_USER" 1
+    _debug "Detected ENV variable PANOS_USER. Looking for changes..."
+    #Save PANOS_USER as new variable and unset.  Then attempt to load saved variable.
+    PANOS_ENV_USER="$PANOS_USER"
+    unset PANOS_USER
+    _getdeployconf PANOS_USER
+    # Check if saved key exists, or if saved key differs from ENV variable
+    if [ -z "$PANOS_USER" ] || { [ "$PANOS_USER" ] && [ "$PANOS_ENV_USER" != "$PANOS_USER" ]; }; then
+      _debug "PANOS_USER has changed.  Saving changes to disk."
+      _savedeployconf PANOS_USER "$PANOS_ENV_USER" 1
+      _regen_keys=true # Regenerate keys if the username changes
+      PANOS_USER="$PANOS_ENV_USER"
+    else
+      _debug "PANOS_USER is unchanged."
+    fi
   else
     _debug "Attempting to load variable PANOS_USER from file."
     _getdeployconf PANOS_USER
   fi
 
-  # PANOS_PASS
+  ## PANOS_PASS
   if [ "$PANOS_PASS" ]; then
-    _debug "Detected ENV variable PANOS_PASS. Saving to file."
-    _savedeployconf PANOS_PASS "$PANOS_PASS" 1
+    _debug "Detected ENV variable PANOS_PASS. Looking for changes..."
+    #Save variable as temp
+    PANOS_ENV_PASS="$PANOS_PASS"
+    unset PANOS_PASS
+    #Attempt to load saved variable
+    _getdeployconf PANOS_PASS
+    if [ -z "$PANOS_PASS" ] || { [ "$PANOS_PASS" ] && [ "$PANOS_ENV_PASS" != "$PANOS_PASS" ]; }; then
+      #if [ "$PANOS_ENV_PASS" != "$PANOS_PASS" ]; then
+      _debug "PANOS_PASS has changed.  Saving changes to disk."
+      _savedeployconf PANOS_PASS "$PANOS_ENV_PASS" 1
+      _regen_keys=true #Regenerate keys if the password changes
+      PANOS_PASS="$PANOS_ENV_PASS"
+    else
+      _debug "PANOS_PASS is unchanged."
+    fi
   else
     _debug "Attempting to load variable PANOS_PASS from file."
     _getdeployconf PANOS_PASS
   fi
 
-  # PANOS_KEY
-  _getdeployconf PANOS_KEY
-  if [ "$PANOS_KEY" ]; then
-    _debug "Detected saved key."
-    _panos_key=$PANOS_KEY
-  else
-    _debug "No key detected"
-    unset _panos_key
-  fi
-
   #Store variables
-  _panos_host=$PANOS_HOST
   _panos_user=$PANOS_USER
   _panos_pass=$PANOS_PASS
 
-  #Test API Key if found.  If the key is invalid, the variable _panos_key will be unset.
-  if [ "$_panos_host" ] && [ "$_panos_key" ]; then
-    _debug "**** Testing API KEY ****"
-    deployer keytest
-  fi
-
   # Check for valid variables
-  if [ -z "$_panos_host" ]; then
+  if [ -z "$PANOS_HOST" ]; then
     _err "No host found. If this is your first time deploying, please set PANOS_HOST in ENV variables. You can delete it after you have successfully deployed the certs."
     return 1
   elif [ -z "$_panos_user" ]; then
@@ -195,21 +228,71 @@ panos_deploy() {
     _err "No password found. If this is your first time deploying, please set PANOS_PASS in ENV variables. You can delete it after you have successfully deployed the certs."
     return 1
   else
-    # Generate a new API key if no valid API key is found
-    if [ -z "$_panos_key" ]; then
-      _debug "**** Generating new PANOS API KEY ****"
-      deployer keygen
-      _savedeployconf PANOS_KEY "$_panos_key" 1
+
+    #######################################################
+    ### Loop through all current hosts and deploy certs ###
+    #######################################################
+    _info "**** Deploying certs to hosts ****"
+    echo "$PANOS_HOST" | while read -r _panos_host; do
+      _debug ""
+      _debug "**************************************************"
+      _debug "*****  PROCESSING HOST: $_panos_host  *****"
+      _debug "**************************************************"
+
+      # Use MD5 to generate unique API key name suffixes, retreive API key from file and store as variable _panos_key
+      md5suffix=$(echo "$_panos_host" | md5sum | awk '{print $1}')
+      host_key_name="PANOS_KEY_$md5suffix"
+      _getdeployconf "$host_key_name"
+      eval "_panos_key=\${$host_key_name}"
+
+      # Test API key.  If the key is invalid, the variable _panos_key will be unset.
+      if [ "$_panos_key" ]; then
+        _debug "**** Testing API KEY ****"
+        deployer keytest
+      fi
+
+      # Generate a new API key if no valid API key is found, or if _regen_keys is true
+      if [ -z "$_panos_key" ] || [ "$_regen_keys" = true ]; then
+        _debug "**** Generating new PANOS Host API KEY ****"
+        deployer keygen
+        _savedeployconf "$host_key_name" "$_panos_key" 1
+      fi
+
+      # Confirm that a valid key was generated
+      if [ -z "$_panos_key" ]; then
+        _err ""
+        _err "Unable to generate an API key.  The user and pass may be invalid or not authorized to generate a new key. "
+        _err "Please check the PANOS_USER and PANOS_PASS credentials and try again."
+        _err "If your credentials are valid, you may need to use the --insecure flag."
+        return 1
+      else
+        deployer cert
+        deployer key
+        deployer commit
+      fi
+    done
+    _info "**** Finished deploying certs to host(s) ****"
+
+    #########################################################################
+    ### Generate a list of deleted hosts and delete orphan keys from conf ###
+    #########################################################################
+    if [ "$_old_host" ] && [ "$_old_host" != "$PANOS_HOST" ]; then
+      _debug "**************************************************"
+      _debug "***** Removing orphan keys for deleted hosts *****"
+      _debug "**************************************************"
+      #Loop through old and new hosts and find orphan hosts so we can delete their associated keys
+      echo "$_old_host" | while read -r item; do
+        host_to_delete=$(echo "$item" | grep -v "$PANOS_HOST")
+        if [ "$host_to_delete" ]; then
+          #find the associated key name and clear from domain config file
+          md5suffix=$(echo "$host_to_delete" | md5sum | awk '{print $1}')
+          host_key_name="SAVED_PANOS_KEY_$md5suffix"
+          _debug "*** Deleting saved key for host: $host_to_delete ***"
+          _cleardomainconf "$host_key_name"
+        fi
+      done
+      _info "**** Finished removing orphan keys ****"
     fi
 
-    # Confirm that a valid key was generated
-    if [ -z "$_panos_key" ]; then
-      _err "Unable to generate an API key.  The user and pass may be invalid or not authorized to generate a new key.  Please check the PANOS_USER and PANOS_PASS credentials and try again"
-      return 1
-    else
-      deployer cert
-      deployer key
-      deployer commit
-    fi
   fi
 }
